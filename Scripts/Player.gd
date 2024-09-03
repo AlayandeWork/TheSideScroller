face93615e89a185fe0af5360015cd51a7c8850d
AbdulMatin Alayande Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-09-02 23:22:13 -0400

    update

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 # Player Variables
 var speed = 190
-var jumpPower = 245
+var jumpPower = 350
 var gravity = 1000
 
 func _physics_process(delta):
@@ -16,17 +16,28 @@ func _physics_process(delta):
 	# Player movement Left & Right
 	var playerDirection = Input.get_axis("Left","Right")
 	
-	if playerDirection:
-		velocity.x = speed * playerDirection
-	else:
-		velocity.x = 0
-		
-		
-		
-	# Player Jump
+	
+	# On the floor and not on the floor
 	if is_on_floor():
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y -= jumpPower
+			$AnimationPlayer.play("Jump")
+		elif playerDirection:
+			velocity.x = speed * playerDirection
+			if playerDirection > 0:
+				$AnimationPlayer.play("rightRun")
+			else:
+				$AnimationPlayer.play("leftRun")
+		else:
+			velocity.x = 0
+			$AnimationPlayer.play("Idle")
+	else:
+		if velocity.y > 0:
+			$AnimationPlayer.play("Fall")
+		else:
+			$AnimationPlayer.play("Jump")
+		
+	
 			
 			
 	move_and_slide()
